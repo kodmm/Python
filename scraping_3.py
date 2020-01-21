1f1e9369b7a06049b380e6c835e981f02e054c5c
kodmm <tawaratsumida.kosei.infratop@gmail.com>
2020-01-21 12:25:00 +0900

    In the middle of weather scraping

--- scraping_3.py
+++ scraping_3.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.accuweather.com/en/jp/japan-weather")
@@ -8,9 +9,16 @@ search_word = driver.find_element_by_name("query")
 search_word.send_keys("Saitama-shi")
 search_word.submit()
 
-currrent_temp = drive.find_element_by_class_name("high")
-condition = driver.find_element_by_class_name("cond")
+current_temp = []
+condition = []
+time.sleep(3)
+current_temp = driver.find_elements_by_class_name("high")
+condition = driver.find_elements_by_class_name("cond")
 
-for name in zip(current_temp, conditon):
-    print(name)
+print ('Condition   : ' + (condition.text))
 
+for date, condition in zip(current_temp, condition):
+    print(date, condition)
+
+driver.close()
+driver.quit()
